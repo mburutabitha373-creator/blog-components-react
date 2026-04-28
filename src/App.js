639e451fda385c3460d58938d5bb01f41e405015
Tabitha Mburu <mburutabitha373@gmail.com>
2026-04-28 12:38:39 +0300

    Fix App.js compatibility for tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import blog from "./blog";
-
-import Header from "./components/Header";
-import About from "./components/About";
-import ArticleList from "./components/ArticleList";
 
 function App() {
-  return (
-    <div>
-      <a href="https://react.dev">learn react</a>
-    </div>
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "a",
+      { href: "https://react.dev" },
+      "learn react"
+    )
   );
 }
 
