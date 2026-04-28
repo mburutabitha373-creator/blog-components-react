4f1851ba7258cc83f051826e64ae9f4523494651
Tabitha Mburu <mburutabitha373@gmail.com>
2026-04-28 12:21:01 +0300

    Finish React blog components lab

---
+++ src/blog.js
@@ -0,0 +1,21 @@
+const blog = {
+  name: "My Personal Blog",
+  image: "https://via.placeholder.com/150",
+  about: "Welcome to my blog where I share my thoughts and experiences.",
+  posts: [
+    {
+      id: 1,
+      title: "My First Post",
+      date: "2026-04-20",
+      preview: "This is the preview of my first post..."
+    },
+    {
+      id: 2,
+      title: "Learning React",
+      date: "2026-04-21",
+      preview: "React is fun once you understand components..."
+    }
+  ]
+};
+
+export default blog;
