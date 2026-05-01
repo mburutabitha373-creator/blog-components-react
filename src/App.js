d6721df6ba02f91dedfa8a1f1ec7774eda1d7598
Tabitha Mburu <mburutabitha373@gmail.com>
2026-05-01 10:51:51 +0300

    Fix blog components and pass all tests

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import About from "./components/About";
 import ArticleList from "./components/ArticleList";
 
 function App() {
-  const articles = blog.articles;
-
   return (
     <div>
       <Header name={blog.name} />
       <About image={blog.image} about={blog.about} />
-      <ArticleList posts={articles} />
+      <ArticleList posts={blog.articles} />
     </div>
   );
 }
